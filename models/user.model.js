77ddbc0d53b8f8fe39de8d9063efda35ecb94dd2
Abhay Singh <55654179+DanteHaxor@users.noreply.github.com>
2023-01-17 21:13:59 +0530

    Delete models directory

--- models/user.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-const userSchema=mongoose.Schema({
-    firstname:String,
-    lastname:String,
-    country:String,
-    dob:String,
-    email:String,
-    password:String
-})
