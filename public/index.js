e9c16183accaa50d0c8c044bae72d9bb46ca067e
Abhay Singh <55654179+DanteHaxor@users.noreply.github.com>
2023-01-17 21:13:43 +0530

    Delete public directory

--- public/index.js
+++
@@ -1,7 +0,0 @@
-async function test(){
-    let res=await fetch("https://dulcet-licorice-2602ef.netlify.app/users")
-    let data=await res.json()
-    console.log(data);
-    document.getElementById("test").innerHTML=data.msg
-}
-test()
